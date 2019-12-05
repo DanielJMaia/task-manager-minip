bc0ca3e239152ebcb8e3ebce18838dd361b54c34
DanielJMaia <djohnsonmaia@gmail.com>
2019-12-05 18:33:43 +0000

    copied files from github, not working

--- app.py
+++ app.py
@@ -3,28 +3,32 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = "task_manager"
-app.config["MONGO_URI"] = "mongodb+srv://root:r00tUser@myfirstcluster-bgp7f.mongodb.net/task_manager?retryWrites=true&w=majority"
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
-@app.route("/")
-@app.route("/get_tasks")
+@app.route('/')
+@app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", 
+                           tasks=mongo.db.tasks.find())
 
-@app.route("/add_task")
+
+@app.route('/add_task')
 def add_task():
-    return render_template("addtask.html",
-    categories=mongo.db.categories.find())
-    
-@app.route("/insert_task", method=["POST"])
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
 def insert_task():
-    tasks = mongo.db.tasks
+    tasks =  mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
+
 
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
@@ -33,7 +37,8 @@ def edit_task(task_id):
     return render_template('edittask.html', task=the_task,
                            categories=all_categories)
 
-@app.route('update_task<task_id>', method=["POST"])
+
+@app.route('/update_task/<task_id>', methods=["POST"])
 def update_task(task_id):
     tasks = mongo.db.tasks
     tasks.update( {'_id': ObjectId(task_id)},
@@ -47,8 +52,7 @@ def update_task(task_id):
     return redirect(url_for('get_tasks'))
 
 
-    
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=True)
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
